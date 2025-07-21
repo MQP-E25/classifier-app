8d3934a4e9f522e3012b0fa78f27ada4baa08678
rempixel <yjiang00634@gmail.com>
2025-07-21 15:41:42 -0400

    debug remote server issue.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,7 +7,7 @@ import { View } from '@/components/Themed';
 import StyledCard from '@/components/StyledCard';
 import CsvUpload from '@/components/CsvUpload';
 
-const REMOTE_SERVER = 'http://130.215.43.73/analyzeNotebook';
+const REMOTE_SERVER = 'https://130.215.43.73/analyzeNotebook';
 
 export default function TabOneScreen() {
   const [resultData, setResultData] = useState<any>(null);
